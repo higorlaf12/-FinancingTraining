418c7917d16966e9226d08fb872ab33e368f42b5
HigorLucas <higor.freitas@neppoadm.com.br>
2019-02-06 09:44:33 -0200

    Primeiro Passo colocar o projeto no git

---
+++ src/main/java/com/FinanciamenteCRM/Finaciamente/FinaciamenteApplication.java
@@ -0,0 +1,14 @@
+package com.FinanciamenteCRM.Finaciamente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaciamenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaciamenteApplication.class, args);
+	}
+
+}
+
